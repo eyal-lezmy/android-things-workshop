ce51de240102338d276ae8434053f02c3fd9cc4b
Gautier Nilhcem <nilhcem@gmail.com>
2018-03-16 17:00:46 +0100

    Update MainActivity comment to the latest version in the template

--- app/src/main/java/com/example/androidthings/myproject/MainActivity.java
+++ app/src/main/java/com/example/androidthings/myproject/MainActivity.java
@@ -8,22 +8,23 @@ import com.google.android.things.contrib.driver.pwmservo.Servo;
 import com.google.android.things.pio.Gpio;
 
 /**
- * Skeleton of the main Android Things activity. Implement your device's logic
- * in this class.
+ * Skeleton of an Android Things activity.
  * <p>
  * Android Things peripheral APIs are accessible through the class
- * PeripheralManagerService. For example, the snippet below will open a GPIO pin and
+ * PeripheralManager. For example, the snippet below will open a GPIO pin and
  * set it to HIGH:
  * <p>
  * <pre>{@code
- * PeripheralManagerService service = new PeripheralManagerService();
- * mLedGpio = service.openGpio("BCM6");
+ * PeripheralManager manager = PeripheralManager.getInstance();
+ * mLedGpio = manager.openGpio("BCM6");
  * mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
  * mLedGpio.setValue(true);
  * }</pre>
  * <p>
  * For more complex peripherals, look for an existing user-space driver, or implement one if none
  * is available.
+ *
+ * @see <a href="https://github.com/androidthings/contrib-drivers#readme">https://github.com/androidthings/contrib-drivers#readme</a>
  */
 public class MainActivity extends Activity {
 
